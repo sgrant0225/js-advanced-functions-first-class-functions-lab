9994231f2559de246b60c389efb08fe481dda6df
github username <sharnellgrant@gmail.com>
2021-03-06 15:54:27 -0500

    commit tree 5460afb3a7797f62e4c157697f5b4ed96cd130d7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr_drivers){
+    return arr_drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
+   
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+  const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
+// selectDifferentDrivers() — This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() 
+//or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return 
+//either the first two drivers or the last two drivers
